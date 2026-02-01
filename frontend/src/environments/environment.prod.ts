fd69cfaf49dd5e6215843f07a8c655de7a753431
Germain R. Bueno T <gerbless@gmail.com>
2026-02-01 15:36:48 -0300

    chore: Update API URL to Render deployment in both development and production environments.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000'
-    // apiUrl: 'https://padel-tournament-ide6.onrender.com'
+    //apiUrl: 'http://localhost:3000'
+    apiUrl: 'https://padel-tournament-ide6.onrender.com'
 };
