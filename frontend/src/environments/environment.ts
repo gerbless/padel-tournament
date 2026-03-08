4879f16cbaa090a0f3d189a31df0eaa76e7ddf86
Germain R. Bueno T <gerbless@gmail.com>
2026-03-07 21:30:21 -0300

    feat: update Twilio credentials in environment files and set active API URL

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     //apiUrl: 'https://padel-tournament-ide6.onrender.com'
-    //apiUrl: 'https://padel-tournament-4gwr.onrender.com/api'
+    apiUrl: 'https://padel-tournament-4gwr.onrender.com/api'
     //apiUrl: 'http://localhost/api'
-    apiUrl: 'https://limier-stubbornly-dania.ngrok-free.dev/api'
+    //apiUrl: 'https://limier-stubbornly-dania.ngrok-free.dev/api'
 };
