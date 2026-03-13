db89e53b5d799f7f1b72b805abed5e48c01972d1
Germain R. Bueno T <gerbless@gmail.com>
2026-03-13 13:16:07 -0300

    fix: correct apiUrl configuration for local development environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,6 @@ export const environment = {
     production: false,
     //apiUrl: 'https://padel-tournament-ide6.onrender.com'
     //apiUrl: 'https://padel-tournament-4gwr.onrender.com/api'
-    //apiUrl: 'http://localhost/api'
+    apiUrl: 'http://localhost:3000/api'
     //apiUrl: 'https://limier-stubbornly-dania.ngrok-free.dev/api'
-    apiUrl: 'http://localhost:3000'
 };
