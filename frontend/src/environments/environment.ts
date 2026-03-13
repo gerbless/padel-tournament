4ff41826db4e9ed88f103ab6fdfc507d23dd700a
Germain R. Bueno T <gerbless@gmail.com>
2026-03-13 12:43:46 -0300

    fix: update apiUrl to use localhost for development environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: false,
     //apiUrl: 'https://padel-tournament-ide6.onrender.com'
-    apiUrl: 'https://padel-tournament-4gwr.onrender.com/api'
+    //apiUrl: 'https://padel-tournament-4gwr.onrender.com/api'
     //apiUrl: 'http://localhost/api'
     //apiUrl: 'https://limier-stubbornly-dania.ngrok-free.dev/api'
+    apiUrl: 'http://localhost:3000/api'
 };
