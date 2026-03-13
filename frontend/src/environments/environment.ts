eac732269aacd34bd6a31c8c4bfef58622ee4399
Germain R. Bueno T <gerbless@gmail.com>
2026-03-13 13:19:25 -0300

    fix: update apiUrl to point to the correct production endpoint

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     //apiUrl: 'https://padel-tournament-ide6.onrender.com'
-    //apiUrl: 'https://padel-tournament-4gwr.onrender.com/api'
-    apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'https://padel-tournament-4gwr.onrender.com/api'
+    //apiUrl: 'http://localhost:3000/api'
     //apiUrl: 'https://limier-stubbornly-dania.ngrok-free.dev/api'
 };
